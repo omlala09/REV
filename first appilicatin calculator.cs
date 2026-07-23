aea9cd46b7df851a54921f8a2b7072cb50176f80
om srivastava <omsrivastav5605@gmail.com>
2026-07-23 11:41:30 +0530

    Add files via upload

---
+++ first appilicatin calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("First Number: ");
+            int first = Int32.Parse(Console.ReadLine());
+            Console.Write("Second Number: ");
+            int Second= Int32.Parse(Console.ReadLine());
+            int result= first + Second;
+            Console.WriteLine(first + "+"+ Second+"="+result);
+            result = first - Second;
+            Console.WriteLine(first + "-"+ Second +"="+result);
+            result = first / Second;
+            Console.WriteLine(first + "/" + Second + "=" + result);
+            result = first * Second;
+            Console.WriteLine(first + "*" + Second + "=" + result);
+        }
+    }
+}
+
