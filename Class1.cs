316a7c8c970589153db2dfc3ffb43acad471dbbe
om srivastava <omsrivastav5605@gmail.com>
2026-07-24 17:29:11 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hello
+{
+    class Player
+    {
+        public string name = "OM";
+        public int health = 49;
+
+        public void setHealth(int h)
+        {
+            health = h;
+        }
+    }
+}
